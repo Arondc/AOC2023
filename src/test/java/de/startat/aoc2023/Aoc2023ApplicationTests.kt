15f55899a67e4b42d8d263b57aa6004746fbb1a6
Sten Seifert <sten.seifert@googlemail.com>
2023-12-04 21:01:32 +0100

    Switch to Kotlin (accidentally created the project with java -_-)

--- src/test/java/de/startat/aoc2023/Aoc2023ApplicationTests.kt
+++ src/test/java/de/startat/aoc2023/Aoc2023ApplicationTests.kt
@@ -1,13 +1,11 @@
-package de.startat.aoc2023;
+package de.startat.aoc2023
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
 
 @SpringBootTest
 class Aoc2023ApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
+    @Test
+    fun contextLoads() {
+    }
 }
