92430086c8096f0eed23f001e261db5448c5252c
Sten Seifert <sten.seifert@googlemail.com>
2023-12-04 21:16:19 +0100

    Make main class a command line app

--- src/main/kotlin/de/startat/aoc2023/Aoc2023Application.kt
+++ src/main/kotlin/de/startat/aoc2023/Aoc2023Application.kt
@@ -1,10 +1,17 @@
 package de.startat.aoc2023
 
+import org.springframework.boot.CommandLineRunner
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import kotlin.system.exitProcess
 
 @SpringBootApplication
-class Aoc2023Application
+class Aoc2023Application : CommandLineRunner {
+	override fun run(vararg args: String?) {
+		println("Hello AOC2023")
+		exitProcess(0)
+	}
+}
 
 fun main(args: Array<String>) {
 	runApplication<Aoc2023Application>(*args)
